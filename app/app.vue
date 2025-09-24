f3ebc84411af73a4af730815b3eea4027169d60d
bhkim <bhkim@idevel.kr>
2025-09-24 22:56:36 +0900

    :update:search_console_naver_search_adviser

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'google-site-verification', content: 'vLf2i6BUZHTCPafMegoqrxNwxr7vLMGEc1Wtq983TDI' },
+    { name: 'naver-site-verification', content: '1e355047b6b84e533419b1d73b9e5eb475862599' }
+  ]
+})
+</script>
+
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtPage />
 </template>
